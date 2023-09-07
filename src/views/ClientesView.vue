142588deccd37538160bfaad8335438a1bbca6ee
Oscar Meza <a3519110251@alumno.uttehuacan.edu.mx>
2023-09-07 16:56:15 -0600

    first commit

---
+++ src/views/ClientesView.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import RouterLink from '../components/Ui/RouterLink.vue';
+  import Heading from '../components/UI/Heading.vue';
+  defineProps({
+    titulo: {
+      type: String
+    }
+  })
+  </script>
+
+<template>
+  <div>
+    <div class="flex justify-end">
+      <RouterLink to="agregar-cliente">
+        Agregar Cliente
+      </RouterLink>
+    </div>
+    <Heading>{{ titulo }}</Heading>
+  </div>
+</template>
---
+++ src/views/NuevoClienteView.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { FormKit } from "@formkit/vue";
+import RouterLink from "../components/Ui/RouterLink.vue";
+import Heading from "../components/UI/Heading.vue";
+defineProps({
+  titulo: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <div class="flex justify-end">
+      <RouterLink to="inicio"> Volver </RouterLink>
+    </div>
+    <Heading>{{ titulo }}</Heading>
+    <div class="mx-auto mt-10 bg-white shadow">
+      <div class="mx-auto md:w-2/3 py-20 px-6">
+        <FormKit type="form">
+          <FormKit
+            type="text"
+            label="Nombre"
+            placeholder="Nombre de Cliente"
+            validation="required"
+            :validation-messages="{
+              required: 'El nombre del cliente es obligatorio',
+            }"
+          />
+
+          <FormKit
+            type="text"
+            label="Apellido"
+            placeholder="Apellido de Cliente"
+            validation="required"
+            :validation-messages="{
+              required: 'El apellido del cliente es obligatorio',
+            }"
+          />
+
+          <FormKit
+            type="email"
+            label="Email"
+            placeholder="Email de Cliente"
+            validation="required|email"
+            :validation-messages="{
+              required: 'El email del cliente es obligatorio',
+              email: 'Ingrese un email válido'
+            }"
+          />
+
+          <FormKit
+            type="text"
+            label="Teléfono"
+            placeholder="Teléfono: XXX-XXX-XXXX"
+            validation="*matches:/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/"
+            :validation-messages="{ matches: 'El formato no es válido' }"
+          />
+        </FormKit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+    .formkit-wrapper {
+        max-width: 100%;
+    }
+</style>
